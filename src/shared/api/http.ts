49d70824624acb5c3417066de8cdcb556ebfbe23
Bogdan Safronov <bogdasafronov@gmail.com>
2024-02-19 17:42:07 +0300

    feat(auth): add interceptors

--- src/shared/api/http.ts
+++ src/shared/api/http.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../hooks";
 
 export const api = axios.create({
   baseURL: import.meta.env.BASE_URL,
@@ -12,4 +13,47 @@ export const apiAuth = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use(async (config) => {
+  const auth = useAuthStore.getState();
+
+  if (auth.isAuth && config.headers) {
+    config.headers["Authorization"] = `Bearer ${auth.accessToken}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(undefined, async (error) => {
+  const originalRequest = error.config;
+  const auth = useAuthStore.getState();
+
+  if (error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+
+    if (!auth.isAuth) {
+      auth.logout();
+      return Promise.reject(error);
+    }
+
+    try {
+      // TODO: implement refresh flow
+      // const { data } = await auth.refresh({
+      //   refresh: authState.refreshToken,
+      // })
+
+      // auth.refresh({
+      //   accessToken: data.access,
+      //   refreshToken: data.refresh,
+      // })
+
+      return api(originalRequest);
+    } catch (error) {
+      auth.logout();
+      return Promise.reject(error);
+    }
+  }
+
+  return Promise.reject(error);
 });

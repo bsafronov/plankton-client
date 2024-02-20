65016c913de50a9373bf5c49385dd4d5acf28be4
Bogdan Safronov <bogdasafronov@gmail.com>
2024-02-20 11:37:18 +0300

    feat(router): add mock routes

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,15 +1,57 @@
 import { createBrowserRouter } from "react-router-dom";
-import { RootProvider } from "./providers/root-provider";
-import { HomePage } from "~/pages";
+import { RootLayout } from "./layouts/root-layout";
+import { AuthLayout } from "./layouts/auth-layout";
+import { DashboardLayout } from "./layouts/dashboard-layout";
+import { ErrorPage } from "~/pages/error";
+import { HomePage } from "~/pages/page";
+import { SignInPage } from "~/pages/auth/sign-in/page";
+import { SignUpPage } from "~/pages/auth/sign-up/page";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootProvider />,
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: <HomePage />,
+        element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <HomePage />,
+          },
+          {
+            path: "/profile",
+            element: <div>Profile</div>,
+          },
+          {
+            path: "/messages",
+            element: <div>Messages</div>,
+          },
+          {
+            path: "/settings",
+            element: <div>Settings</div>,
+          },
+          {
+            path: "/processes",
+            element: <div>Processes</div>,
+          },
+        ],
+      },
+      {
+        path: "/auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/auth/sign-in",
+            element: <SignInPage />,
+          },
+          {
+            path: "/auth/sign-up",
+            element: <SignUpPage />,
+          },
+        ],
       },
     ],
   },

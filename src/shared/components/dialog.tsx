9bb637db2bd868e646abdcec9f60b98b890c7db4
Bogdan Safronov <bogdasafronov@gmail.com>
2024-02-20 11:46:25 +0300

    feat(ui): add reusable dialog composition

---
+++ src/shared/components/dialog.tsx
@@ -0,0 +1,43 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
+type Props = React.ComponentPropsWithoutRef<typeof Dialog> & {
+  trigger: React.ReactNode;
+  title?: string;
+  description?: string;
+  footer?: React.ReactNode;
+};
+
+export const CDialog = ({
+  children,
+  trigger,
+  title,
+  description,
+  footer,
+  ...props
+}: Props) => {
+  return (
+    <Dialog {...props}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          {title && <DialogTitle>{title}</DialogTitle>}
+          {description && <DialogDescription>{description}</DialogDescription>}
+        </DialogHeader>
+        {children}
+        <DialogFooter>
+          {footer}
+          <DialogClose>Закрыть</DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};

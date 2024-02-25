2dd8d89d049d59a68c8b7901ac2d45c77089df83
Bogdan <bogdasafronov@gmail.com>
2024-02-25 18:42:54 +0300

    feat(auth): add sign-up form widget

--- src/features/sign-up/hooks/use-form.tsx
+++ src/features/sign-up/hooks/use-form.tsx
@@ -2,11 +2,30 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
-const schema = z.object({
-  username: z.string(),
-  password: z.string(),
-  confirmPassword: z.string(),
-});
+const schema = z
+  .object({
+    username: z
+      .string()
+      .min(5, { message: "Минимальная длина имени пользователя 5 символов" }),
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string(),
+    password: z
+      .string()
+      .min(6, { message: "Минимальная длина пароля 6 символов" }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Минимальная длина пароля 6 символов" }),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Пароли не совпадают",
+        path: ["confirmPassword"],
+      });
+    }
+  });
 
 type Schema = z.infer<typeof schema>;
 
@@ -15,6 +34,9 @@ export const useSignUpForm = () => {
     resolver: zodResolver(schema),
     defaultValues: {
       username: "",
+      firstName: "",
+      lastName: "",
+      email: "",
       password: "",
       confirmPassword: "",
     },
--- src/features/sign-up/ui/form.tsx
+++ src/features/sign-up/ui/form.tsx
@@ -1,3 +1,56 @@
+import { CForm, CFormField } from "~/shared/components";
+import { Input, InputPassword } from "~/shared/ui";
+import { useSignUpForm } from "../hooks/use-form";
+
 export const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  const form = useSignUpForm();
+  const { handleSubmit, control } = form;
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data);
+  });
+
+  return (
+    <CForm form={form} onSubmit={onSubmit} submitText="Зарегистрироваться">
+      <CFormField
+        control={control}
+        name="username"
+        label="Логин"
+        required
+        render={(props) => <Input {...props} />}
+      />
+      <CFormField
+        control={control}
+        name="password"
+        label="Пароль"
+        required
+        render={(props) => <InputPassword {...props} />}
+      />
+      <CFormField
+        control={control}
+        name="confirmPassword"
+        label="Подтверждение пароля"
+        required
+        render={(props) => <InputPassword {...props} />}
+      />
+      <CFormField
+        control={control}
+        name="firstName"
+        label="Имя"
+        render={(props) => <Input {...props} />}
+      />
+      <CFormField
+        control={control}
+        name="lastName"
+        label="Фамилия"
+        render={(props) => <Input {...props} />}
+      />
+      <CFormField
+        control={control}
+        name="email"
+        label="Почта"
+        render={(props) => <Input {...props} />}
+      />
+    </CForm>
+  );
 };

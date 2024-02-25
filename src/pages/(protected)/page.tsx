75e3a3561d8236c3d24dace4c4c9d6d18a486860
Bogdan <bogdasafronov@gmail.com>
2024-02-25 18:45:17 +0300

    feat(ui): add root authorized page

--- src/pages/(protected)/page.tsx
+++ src/pages/(protected)/page.tsx
@@ -1,3 +1,13 @@
+import { Package } from "lucide-react";
+
 export const RootPage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center flex-col">
+      <Package className="size-40 text-muted-foreground" />
+      <h1 className="text-5xl font-bold">Главная страница</h1>
+      <h5 className="text-muted-foreground font-semibold text-lg">
+        Здесь можно отобразить основную информацию с разных страниц
+      </h5>
+    </div>
+  );
 };

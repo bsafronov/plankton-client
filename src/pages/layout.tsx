0208f0979dc27c4c3005437da7bdf4776c3a6b89
Bogdan <bogdasafronov@gmail.com>
2024-02-25 18:43:50 +0300

    feat(ui): add tooltop provider

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,14 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet } from "react-router-dom";
-import { Toaster } from "~/shared/ui";
+import { Toaster, TooltipProvider } from "~/shared/ui";
 
 const queryClient = new QueryClient();
 
 export const RootLayout = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Outlet />
+      <TooltipProvider>
+        <Outlet />
+      </TooltipProvider>
       <ReactQueryDevtools />
       <Toaster richColors />
     </QueryClientProvider>

46abaf822afdd3d9e52e77532578b28e0e45dcdb
Bogdan <bogdasafronov@gmail.com>
2024-02-25 18:43:36 +0300

    refactor(auth): temp change to mock login

--- src/features/sign-in/ui/form.tsx
+++ src/features/sign-in/ui/form.tsx
@@ -1,42 +1,46 @@
 import { toast } from "sonner";
-import { CFormField } from "~/shared/components";
+import { CForm, CFormField } from "~/shared/components";
+import { Input, InputPassword } from "~/shared/ui";
 import { useSignInForm } from "../hooks/use-form";
-import { useSignInMutation } from "../hooks/use-mutation";
-import { Button, Form, Input, InputPassword } from "~/shared/ui";
+// import { useSignInMutation } from "../hooks/use-mutation";
+import { useAuthStore } from "~/shared/hooks";
 
 export const SignInForm = () => {
   const form = useSignInForm();
   const { control, handleSubmit } = form;
-  const { mutateAsync } = useSignInMutation();
-
+  // const { mutateAsync } = useSignInMutation();
+  const login = useAuthStore().login;
   const onSubmit = handleSubmit(async (data) => {
     try {
-      const { data: tokens } = await mutateAsync(data);
-      console.log(tokens);
+      // const { data: tokens } = await mutateAsync(data);
+      // console.log(tokens);
+      console.log(data);
+
+      login({
+        accessToken: "",
+        refreshToken: "",
+      });
     } catch (e) {
       toast.error("Неверное имя пользователя или пароль");
     }
   });
 
   return (
-    <Form {...form}>
-      <form onSubmit={onSubmit}>
-        <CFormField
-          control={control}
-          name="username"
-          label="Имя пользователя"
-          render={(props) => <Input {...props} />}
-        />
-        <CFormField
-          control={control}
-          name="password"
-          label="Пароль"
-          render={(props) => <InputPassword {...props} />}
-        />
-        <div className="flex justify-end">
-          <Button>Войти</Button>
-        </div>
-      </form>
-    </Form>
+    <CForm form={form} onSubmit={onSubmit} submitText="Войти">
+      <CFormField
+        control={control}
+        name="username"
+        label="Логин"
+        required
+        render={(props) => <Input {...props} />}
+      />
+      <CFormField
+        control={control}
+        name="password"
+        label="Пароль"
+        required
+        render={(props) => <InputPassword {...props} />}
+      />
+    </CForm>
   );
 };

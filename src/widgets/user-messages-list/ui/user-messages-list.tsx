5d65b3303463fe90ebe6dc165605722900e65c97
Bogdan <bogdasafronov@gmail.com>
2024-02-25 18:34:47 +0300

    feat(message): add messages-list widget

---
+++ src/widgets/user-messages-list/ui/user-messages-list.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { Avatar, AvatarFallback } from "~/shared/ui";
+
+export const UserMessagesListWidget = () => {
+  return (
+    <aside className="border-r">
+      <h5 className="border-b font-semibold text-center">Сообщения</h5>
+      <div className="flex flex-col border-b divide-y">
+        <Link to={"/messages/1"} className="flex p-2 gap-2 items-center">
+          <Avatar>
+            <AvatarFallback className="bg-amber-300" />
+          </Avatar>
+          <div className="flex flex-col text-sm w-40">
+            <span className="font-semibold truncate">Пользователь 1</span>
+            <p className="text-muted-foreground truncate">
+              Последнее сообщение
+            </p>
+          </div>
+        </Link>
+        <Link to={"/messages/2"} className="flex p-2 gap-2 items-center">
+          <Avatar>
+            <AvatarFallback className="bg-emerald-300" />
+          </Avatar>
+          <div className="flex flex-col text-sm w-40">
+            <span className="font-semibold truncate">Пользователь 2</span>
+            <p className="text-muted-foreground truncate">
+              Последнее сообщение
+            </p>
+          </div>
+        </Link>
+      </div>
+    </aside>
+  );
+};

f7ed8acfeba1d90ba25d8feae25b2ab7ef66a7aa
Bogdan Safronov <bogdasafronov@gmail.com>
2024-03-01 17:35:53 +0300

    feat(ui): add cancel button on form component

--- src/shared/components/form/ui/form.tsx
+++ src/shared/components/form/ui/form.tsx
@@ -6,25 +6,38 @@ import { Button, Form } from "~/shared/ui";
 type Props<T extends FieldValues> = {
   form: UseFormReturn<T, any, T>;
   onSubmit: () => void;
+  onCancel?: () => void;
   children?: React.ReactNode;
   className?: string;
+  cancelText?: string;
   submitText?: string;
   isLoading?: boolean;
+  hasCancel?: boolean;
 };
 
 export const CForm = <T extends FieldValues>({
   form,
   onSubmit,
+  onCancel,
   children,
   className,
   submitText = "Отправить",
+  cancelText = "Отмена",
+  hasCancel,
   isLoading,
 }: Props<T>) => {
+  const handleCancel = () => onCancel?.() ?? form.reset();
+
   return (
     <Form {...form}>
       <form onSubmit={onSubmit}>
         <div className={cn("space-y-2", className)}>{children}</div>
-        <div className="flex justify-end mt-4">
+        <div className="flex justify-end mt-4 gap-4">
+          {hasCancel && (
+            <Button variant={"outline"} onClick={handleCancel}>
+              {cancelText}
+            </Button>
+          )}
           <Button disabled={isLoading} className="gap-2">
             {isLoading && (
               <Loader2 className="text-primary-foreground animate-spin" />

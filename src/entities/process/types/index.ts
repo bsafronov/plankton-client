6c47bfa96a01cb86b6a757f257da8cf049fb7c46
Bogdan Safronov <bogdasafronov@gmail.com>
2024-03-12 12:54:14 +0300

    refactor(process): remove non-public types

--- src/entities/process/types/index.ts
+++ src/entities/process/types/index.ts
@@ -3,76 +3,36 @@ export type FieldType = "TEXT" | "NUMBER";
 export type ProcessTemplate = {
   id: ID;
   name: string;
-  fields: ProcessFieldTemplate[];
-  stages: ProcessStageTemplate[];
-  stageFlows: ProcessStageFlowTemplate[];
-  stageFields: ProcessStageFieldTemplate[];
 };
-export type CreateProcessTemplate = Pick<ProcessTemplate, "name">;
 
-export type ProcessFieldTemplate = {
+export type ProcessTemplateField = {
   id: ID;
   name: string;
-  formFields?: ProcessStageFieldTemplate;
   templateId: ID;
 };
-export type CreateProcessFieldTemplate = Pick<
-  ProcessFieldTemplate,
-  "name" | "templateId"
->;
-export type FindManyProcessFieldTemplates = {
-  templateId?: ID;
-};
 
-export type ProcessStageTemplate = {
+export type ProcessTemplateStage = {
   id: ID;
   name: string;
-  fields?: ProcessStageFieldTemplate[];
-  flowsFrom?: ProcessStageFlowTemplate[];
-  flowsTo?: ProcessStageFlowTemplate[];
   templateId: ID;
 };
-export type CreateProcessStageTemplate = Pick<
-  ProcessStageTemplate,
-  "name" | "templateId"
->;
-export type FindManyProcessStageTemplates = {
-  templateId?: ID;
-};
 
-export type ProcessStageFieldTemplate = {
+export type ProcessTemplateStageField = {
   id: ID;
   type: FieldType;
   label?: string;
   description?: string;
   placeholder?: string;
-  flows?: ProcessStageFlowTemplate[];
   stageId: ID;
   fieldId: ID;
   templateId: ID;
 };
-export type CreateProcessStageFieldTemplate = Omit<
-  ProcessStageFieldTemplate,
-  "flows" | "id"
->;
-export type FindManyProcessStageFieldTemplates = {
-  templateId?: ID;
-  stageId?: ID;
-};
 
-export type ProcessStageFlowTemplate = {
+export type ProcessTemplateStageFlow = {
   id: ID;
   value?: string;
   stageId: ID;
   nextStageId: ID;
   fieldId?: ID;
   templateId?: ID;
-};
-export type CreateProcessStageFlowTemplate = Omit<
-  ProcessStageFlowTemplate,
-  "id"
->;
-export type FindManyProcessStageFlowTemplates = {
-  templateId?: ID;
-  stageId?: ID;
 };

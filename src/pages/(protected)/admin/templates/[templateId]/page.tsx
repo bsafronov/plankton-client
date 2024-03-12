975976489ddc944044d9614d379548ff9f7b0500
Bogdan Safronov <bogdasafronov@gmail.com>
2024-03-12 13:45:21 +0300

    refactor(ui): add margin on stage-list widget

--- src/pages/(protected)/admin/templates/[templateId]/page.tsx
+++ src/pages/(protected)/admin/templates/[templateId]/page.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { ProcessTemplateDeleteDialog } from "~/features/delete-process-template";
 import { ProcessTemplateFieldList } from "~/widgets/process-template-field-list";
 import { ProcessTemplateInfo } from "~/widgets/process-template-info";
+import { ProcessTemplateStageList } from "~/widgets/process-template-stage-list";
 
 export const AdminTemplateIdPage = () => {
   const { templateId } = useParams();
@@ -11,6 +12,9 @@ export const AdminTemplateIdPage = () => {
       <div className="grid grid-cols-2 gap-4">
         <ProcessTemplateInfo templateId={id} />
         <ProcessTemplateFieldList templateId={id} />
+      </div>
+      <div className="mt-4">
+        <ProcessTemplateStageList templateId={id} />
       </div>
       <div className="flex justify-end mt-4">
         <ProcessTemplateDeleteDialog templateId={id} />
